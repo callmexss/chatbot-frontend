1026502dbe3fec7f12908171c688c694d61a6f80
callmexss <callmexss@126.com>
2023-08-31 20:24:58 +0800

    perf: better ui for system prompt.

--- app/Components/ToolBar/SystemPromptManager.jsx
+++ app/Components/ToolBar/SystemPromptManager.jsx
@@ -68,18 +68,50 @@ export const SystemPromptManager = ({ setSystemPrompt }) => {
   };
 
   return (
-    <div>
-      <div className="mt-2 max-h-48 overflow-y-auto">
+    <div className="bg-gray-50 p-2 rounded-lg">
+      {/* System Prompt Editing Area */}
+      <div className="mb-4 p-2 rounded-lg bg-white shadow">
+        <input 
+          type="text" 
+          placeholder="Prompt Name" 
+          className="w-full p-2 border rounded mb-1 text-sm"
+          value={editablePrompt.name}
+          onChange={(e) => setEditablePrompt({ ...editablePrompt, name: e.target.value })}
+        />
+        <textarea
+          placeholder="Prompt Content"
+          className="w-full p-2 rounded border mb-1 text-sm"
+          rows="3"
+          value={editablePrompt.content}
+          onChange={(e) => setEditablePrompt({ ...editablePrompt, content: e.target.value })}
+        />
+        <div className="text-xs">
+          <button onClick={addOrUpdatePrompt} className="bg-blue-500 text-white px-3 py-1 rounded shadow">
+            {isEditing ? 'Update' : 'Add'}
+          </button>
+          {isEditing && (
+            <button onClick={() => {
+              setIsEditing(false)
+              setEditablePrompt({ name: '', content: '' })
+            }} className="bg-gray-500 text-white px-3 py-1 rounded shadow ml-2">
+              Cancel
+            </button>
+          )}
+        </div>
+      </div>
+
+      {/* Prompt List */}
+      <div className="mt-2 max-h-48 overflow-y-auto custom-scrollbar">
         {prompts.map((prompt) => (
-          <div key={prompt.id} className="mb-2 flex justify-between items-center">
-            <div className={`text-sm flex-grow ${currentPromptId === prompt.id ? 'text-blue-500' : ''}`}>
+          <div key={prompt.id} className={`mb-1 p-2 rounded-lg ${currentPromptId === prompt.id ? 'bg-blue-100' : 'bg-white'} shadow flex justify-between items-center`}>
+            <div className="text-sm flex-grow text-gray-700">
               <span title={prompt.content}>
                 {prompt.name}
               </span>
             </div>
-            <div className="flex space-x-2">
+            <div className="flex space-x-2 text-xs">
               <button 
-                className={`text-green-500 hover:text-green-700 ${currentPromptId === prompt.id ? 'text-green-900' : ''}`}
+                className="text-green-500 hover:text-green-700"
                 onClick={() => usePrompt(prompt.id, prompt.content)}>
                 Use
               </button>
@@ -93,36 +125,10 @@ export const SystemPromptManager = ({ setSystemPrompt }) => {
           </div>
         ))}
       </div>
-
-      <div className="mt-4">
-        <input 
-          type="text" 
-          placeholder="Prompt Name" 
-          className="w-full p-2 border rounded mb-2"
-          value={editablePrompt.name}
-          onChange={(e) => setEditablePrompt({ ...editablePrompt, name: e.target.value })}
-        />
-        <textarea
-          placeholder="Prompt Content"
-          className="w-full p-2 rounded border mb-2"
-          rows="3"
-          value={editablePrompt.content}
-          onChange={(e) => setEditablePrompt({ ...editablePrompt, content: e.target.value })}
-        />
-        <button onClick={addOrUpdatePrompt} className="bg-blue-500 text-white px-4 py-2 rounded mt-2">
-          {isEditing ? 'Update' : 'Add'}
-        </button>
-        {isEditing && (
-          <button onClick={() => {
-            setIsEditing(false)
-            setEditablePrompt({ name: '', content: '' })
-          }} className="bg-gray-500 text-white px-4 py-2 rounded mt-2 ml-2">
-            Cancel
-          </button>
-        )}
-      </div>
     </div>
   );
+
+
 
 };
 
