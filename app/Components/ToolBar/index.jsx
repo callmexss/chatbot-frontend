e5442247d39cd2252497343e6e20ccb0228d1027
callmexss <callmexss@126.com>
2023-09-20 23:01:06 +0800

    feat: add responsable toolbar.

--- app/Components/ToolBar/index.jsx
+++ app/Components/ToolBar/index.jsx
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import SystemPromptManager from './SystemPromptManager';
 import ConversationManager from './ConversationManager';
 
@@ -9,20 +10,32 @@ export const ToolBar = ({
   conversations,
   setConversations,
 }) => {
+  const [isExpanded, setIsExpanded] = useState(true);
+
+  const toggleExpand = () => {
+    setIsExpanded(!isExpanded);
+  };
+
   return (
-    <div className="flex flex-col w-1/6 p-4 border-r">
+    <div className={`flex flex-col p-4 border-r transition-all duration-300 ${isExpanded ? 'w-full md:w-1/6' : 'w-1/12 md:w-1/12'}`}>
       <h2>Tool Bar</h2>
-      <SystemPromptManager setSystemPrompt={setSystemPrompt} />
-      <ConversationManager 
-        setCurrentConversationId={setCurrentConversationId} 
-        currentConversationId={currentConversationId}
-        setMessages={setMessages} 
-        conversations={conversations}
-        setConversations={setConversations}
-      />
+      <button onClick={toggleExpand}>
+        {isExpanded ? 'Collapse' : 'Expand'}
+      </button>
+      {isExpanded && (
+        <>
+          <SystemPromptManager setSystemPrompt={setSystemPrompt} />
+          <ConversationManager 
+            setCurrentConversationId={setCurrentConversationId} 
+            currentConversationId={currentConversationId}
+            setMessages={setMessages} 
+            conversations={conversations}
+            setConversations={setConversations}
+          />
+        </>
+      )}
     </div>
   );
-
 };
 
 export default ToolBar;
